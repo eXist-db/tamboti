ced834301dac11172777f9bee18ef12ee59cdd25
Adam Retter <adam.retter@googlemail.com>
2011-09-09 17:10:31 +0000

    Updated jQuery datatable code
    svn path=/apps/tamboti/; revision=15334

--- resources/scripts/query.js
+++ resources/scripts/query.js
@@ -739,34 +739,41 @@ function updateSharingDialog() {
 }
 
 //custom fnReloadAjax for sharing dataTable
-function dataTableReloadAjax(oSettings, sNewSource, fnCallback) {
-    if(typeof sNewSource != 'undefined'){
+function dataTableReloadAjax(oSettings, sNewSource, fnCallback, bStandingRedraw) {
+    if(typeof sNewSource != 'undefined' && sNewSource != null) {
         oSettings.sAjaxSource = sNewSource;
     }
 
     this.oApi._fnProcessingDisplay(oSettings, true);
 
     var that = this;
+    var iStart = oSettings._iDisplayStart;
 
-    oSettings.fnServerData( oSettings.sAjaxSource, null, function(json) {
+    oSettings.fnServerData(oSettings.sAjaxSource, [], function(json) {
 
         /* Clear the old information from the table */
-        that.oApi._fnClearTable( oSettings );
+        that.oApi._fnClearTable(oSettings);
+        
         /* Got the data - add it to the table */
-
         for(var i = 0 ; i < json.aaData.length; i++) {
-            that.oApi._fnAddData( oSettings, json.aaData[i] );
+            that.oApi._fnAddData(oSettings, json.aaData[i]);
         }
 
         oSettings.aiDisplay = oSettings.aiDisplayMaster.slice();
-        that.fnDraw(that);
+        that.fnDraw();
+        
+        if(typeof bStandingRedraw != 'undefined' && bStandingRedraw === true) {
+			oSettings._iDisplayStart = iStart;
+			that.fnDraw(false);
+		}
+        
         that.oApi._fnProcessingDisplay(oSettings, false);
 
         /* Callback user function - for event handlers etc */
-        if( typeof fnCallback == 'function'){
+        if(typeof fnCallback == 'function' && fnCallback != null){
             fnCallback(oSettings);
         }
-    });
+    }, oSettings);
 }
 
 //custom rendered for each row of the sharing dataTable
